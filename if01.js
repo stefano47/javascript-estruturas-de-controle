0cfb0bafc4fd1e4484bcbd1c2fab4cf8aef65fc5
Stefano <stefanopaulo47@gmail.com>
2019-06-09 07:48:08 -0300

    estruturas de repeticao: if/else

---
+++ if01.js
@@ -0,0 +1,15 @@
+function soBoaNoticia(nota){
+    if(nota >= 7){
+        console.log(`Aprovado com ${nota}`)
+    }
+}
+
+soBoaNoticia(8)
+
+function seForVerdadeEuFalo(valor){
+    if(valor){
+        console.log(`É verdade... ${valor}`)
+    }
+}
+
+seForVerdadeEuFalo(7)
